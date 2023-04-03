feebcf6cc4a42edfaaf59bfafc669e76eaad1562
chefgordon42069 <rampreethamt@gmail.com>
2023-04-03 19:15:14 +0530

    scrape images ocr

--- src/components/Result.js
+++ src/components/Result.js
@@ -49,6 +49,32 @@ function Result(props) {
       // console.log(event.target.value);
       setTag(event.target.value);
     };
+
+    const ScrImg = async (myurl) => {
+      const link={myurl: myurl};
+      const response = await fetch('http://localhost:4000/api/ocr', {
+        method: 'POST',
+        body: JSON.stringify(link),
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      })
+      .catch(e => {
+        console.log(e);
+      });
+      // console.log(response);
+
+      const json=await response.json();
+      const text=json.data;
+      // console.log(text);
+
+      const element = document.createElement('a');
+      const file = new Blob([text.join('\n')], { type: 'text/plain' });
+      element.href = URL.createObjectURL(file);
+      element.download = 'scraped.txt';
+      document.body.appendChild(element);
+      element.click();
+    }
   
   const itemscom = props.items.map(item => (
     <div key={item.cacheId}>
@@ -56,14 +82,17 @@ function Result(props) {
       <p>{item.snippet}</p>
       <a href={item.link} target="_blank" rel="noreferrer">Link-{item.link}</a>
       <div>
-      <select value={tag}  onChange={handleChange}>
-        {options.map(option => (
-          <option key={option.value} value={option.value}>
-            {option.label}
-          </option>
-        ))}
-      </select>
+        <select value={tag}  onChange={handleChange}>
+          {options.map(option => (
+            <option key={option.value} value={option.value}>
+              {option.label}
+            </option>
+          ))}
+        </select>
         <button onClick={() => {handleButtonClick(item.link)} }>Scrape</button>        
+      </div>
+      <div>
+        <button onClick={ScrImg}>Scrape images</button>        
       </div>
     </div>
   ));
