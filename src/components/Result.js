02303f9b02835f664807a23bbcd585337d5f1928
chefgordon42069 <rampreethamt@gmail.com>
2023-04-19 19:34:35 +0530

    changed folder structure

--- src/components/Result.js
+++ src/components/Result.js
@@ -4,15 +4,25 @@ import { useState } from 'react';
 import Accordion from 'react-bootstrap/Accordion';
 // import { Button } from 'react-bootstrap';
 
-const options = [
-  {value: '', label: 'Select'},
-  { value: 'a', label: 'Links' },
-  { value: 'img', label: 'Images' },
-  { value: 'h', label: 'Headings' }
-]
+import JSZip from 'jszip';
+import FileSaver from 'file-saver';
+
+// const options = [
+//   { value: '', label: 'Select' },
+//   { value: 'a', label: 'Links' },
+//   { value: 'img', label: 'Images' },
+//   { value: 'h', label: 'Headings' }
+// ]
 
 function Result(props) {
-  const [tag, setTag] = useState("");
+
+  const [link, setlink] = useState(false);
+  const [img, setimg] = useState(false);
+  const [head, sethead] = useState(false);
+
+  const [linksList, setlinksList] = useState([]);
+  const [imgsList, setimgsList] = useState([]);
+  const [headsList, setheadsList] = useState([]);
 
   if (!props.items) {
     return <div>Loading...</div>;
@@ -22,61 +32,84 @@ function Result(props) {
 
   const handleButtonClick = async (myurl) => {
 
-    const link = { myurl: myurl, tag: tag };
-    const response = await fetch('https://web-scraper-api-few1.onrender.com/api/', {
-      method: 'POST',
-      body: JSON.stringify(link),
-      headers: {
-        'Content-Type': 'application/json'
-      }
-    })
-      .catch(e => {
-        console.log(e);
-      });
+    const linkBody = { myurl: myurl };
 
-    const json = await response.json();
-    const text = json.data;
-    // console.log(text);
+    if (img) {
+      const response = await fetch('http://localhost:4000/api/img', {
+        method: 'POST',
+        body: JSON.stringify(linkBody),
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      })
+        .catch(e => {
+          console.log(e);
+        });
 
-    const element = document.createElement('a');
-    const file = new Blob([text.join('\n')], { type: 'text/plain' });
-    element.href = URL.createObjectURL(file);
-    element.download = 'scraped.txt';
-    document.body.appendChild(element);
-    element.click();
+      const json = await response.json();
+      const text = json.data;
+      setimgsList(text);
+    }
+
+    if (link) {
+      const response = await fetch('http://localhost:4000/api/links', {
+        method: 'POST',
+        body: JSON.stringify(linkBody),
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      })
+        .catch(e => {
+          console.log(e);
+        });
+
+      const json = await response.json();
+      const text = json.data;
+      setlinksList(text);
+    }
+
+    if (head) {
+      const response = await fetch('http://localhost:4000/api/head', {
+        method: 'POST',
+        body: JSON.stringify(linkBody),
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      })
+        .catch(e => {
+          console.log(e);
+        });
+
+      const json = await response.json();
+      const text = json.data;
+      setheadsList(text);
+    }
+
+    const files = [];
+
+    if(imgsList.length) {
+      files.push({name: "images.txt",data: imgsList.join('\n')});
+    }
+
+    if(linksList.length) {
+      files.push({name: "links.txt",data: linksList.join('\n')});
+    }
+
+    if(headsList.length) {
+      files.push({name: "headings.txt",data: headsList.join('\n')});
+    }
+
+    const zip = new JSZip();
+
+    files.forEach(file => {
+      zip.file(file.name,file.data);
+    });
+
+    zip.generateAsync({ type: "blob" }).then(content => {
+      FileSaver.saveAs(content, "scraped.zip");
+    });
 
   }
-
-  const handleChange = event => {
-    // console.log(event.target.value);
-    setTag(event.target.value);
-  };
-
-  // const ScrImg = async (myurl) => {
-  //   const link = { myurl: myurl };
-  //   const response = await fetch('http://localhost:4000/api/ocr', {
-  //     method: 'POST',
-  //     body: JSON.stringify(link),
-  //     headers: {
-  //       'Content-Type': 'application/json'
-  //     }
-  //   })
-  //     .catch(e => {
-  //       console.log(e);
-  //     });
-  //   // console.log(response);
-
-  //   const json = await response.json();
-  //   const text = json.data;
-  //   // console.log(text);
-
-  //   const element = document.createElement('a');
-  //   const file = new Blob([text.join('\n')], { type: 'text/plain' });
-  //   element.href = URL.createObjectURL(file);
-  //   element.download = 'scraped.txt';
-  //   document.body.appendChild(element);
-  //   element.click();
-  // }
 
   const itemscom = props.items.map(item => (
 
@@ -85,22 +118,17 @@ function Result(props) {
         <Accordion.Item eventKey="1" className='bxx'>
           <Accordion.Header>{item.title}</Accordion.Header>
           <Accordion.Body>
-            
+
             <p>{item.snippet}</p>
             <a href={item.link} target="_blank" rel="noreferrer">Crawl</a>
             <div>
-              <select value={tag} onChange={handleChange}>
-                {options.map(option => (
-                  <option key={option.value} value={option.value}>
-                    {option.label}
-                  </option>
-                ))}
-              </select>
+              <label><input type="checkbox" checked={link} onChange={() => {setlink(!link)}} />Links</label>
+              <label><input type="checkbox" checked={img} onChange={() => {setimg(!img)}} />Images</label>
+              <label><input type="checkbox" checked={head} onChange={() => {sethead(!head)}} />Headings</label>
+              
               <button onClick={() => { handleButtonClick(item.link) }}>Scrape</button>
             </div>
-            {/* <div>
-              <Button onClick={ScrImg} variant="outline-dark">Scrape images</Button>
-            </div> */}
+
           </Accordion.Body>
         </Accordion.Item>
       </Accordion>
